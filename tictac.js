240e8cbd595f1e49f0fbd7e80b8474f9623e55af
Ekaterina Bondareva <1978231@bk.ru>
2023-06-14 16:18:17 +0700

    done2

--- tictac.js
+++ tictac.js
@@ -4,48 +4,44 @@ let field = [
   ["*", "*", "*"],
 ];
 
-function getCrossInput() {
+function getUserInput(symbol) {
   const command = prompt(
-    "Поставьте крестик (Х). Напишите 2 координаты: по y и по x, через запятую"
+    `Куда поставить ${symbol}. Напишите 2 координаты: по y и по x, через запятую`
   );
   const arrayCoordinates = command.split(",");
 
   const x = arrayCoordinates[0];
   const y = arrayCoordinates[1];
   if (field[x][y] === "*") {
-    field[x][y] = "X";
-    if (field[x].every((element) => element === "X")) alert("Победил Х !");
+    field[x][y] = symbol;
+    alert(`${field[0]}\n${field[1]}\n${field[2]}`);
 
-    if (field.every((element) => element[y] === "X")) alert("Победил Х !");
+    if (field[x].every((element) => element === symbol))
+      alert(`Победил ${symbol} !`);
+
+    if (field.every((element) => element[y] === symbol))
+      alert(`Победил ${symbol} !`);
+
+    if (
+      field[0][0] === symbol &&
+      field[1][1] === symbol &&
+      field[2][2] === symbol
+    )
+      alert(`Победил ${symbol} !`);
+    if (
+      field[2][0] === symbol &&
+      field[1][1] === symbol &&
+      field[0][2] === symbol
+    )
+      alert(`Победил ${symbol} !`);
   }
-  if (field[0][0] === "X" && field[1][1] === "X" && field[2][2] === "X")
-    alert("Победил Х !");
-  if (field[2][0] === "X" && field[1][1] === "X" && field[0][2] === "X")
-    alert("Победил Х !");
-  getNilInput();
-}
-
-function getNilInput() {
-  const command = prompt(
-    "Поставьте нолик (0). Напишите 2 координаты: по y и по x, через запятую"
-  );
-  const arrayCoordinates = command.split(",");
-
-  const x = arrayCoordinates[0];
-  const y = arrayCoordinates[1];
-  if (field[x][y] === "*") {
-    field[x][y] = "0";
-    if (field[x].every((element) => element === "0")) alert("Победил 0 !");
-
-    if (field.every((element) => element[y] === "0")) alert("Победил 0 !");
-    if (field[0][0] === "0" && field[1][1] === "0" && field[2][2] === "0")
-      alert("Победил 0 !");
-    if (field[2][0] === "0" && field[1][1] === "0" && field[0][2] === "0")
-      alert("Победил 0 !");
+  if (symbol === "X") {
+    getUserInput("0");
   }
-  getCrossInput();
+  if (symbol === "0") {
+    getUserInput("X");
+  }
 }
 
 console.log(field);
-getCrossInput();
-getNilInput();
+getUserInput("X");
